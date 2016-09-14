7e0de455b4c18a177a13d92f0f8743107fb949b0
Dana van Hassel <dvanhassel@student.tudelft.nl>
2016-09-14 12:56:48 +0200

    comments added and small canges to tests

--- src/test/java/geometry/RectangleTest.java
+++ src/test/java/geometry/RectangleTest.java
@@ -8,6 +8,10 @@ import static org.junit.Assert.assertTrue;
  */
 public class RectangleTest {
 
+    /**
+     * check if a rectangle intersects with a circle
+     * they should intersect, since they are both at position 0,0
+     */
     @Test
     public void intersectCircle(){
         Circle circle = new Circle(6);
@@ -15,16 +19,35 @@ public class RectangleTest {
         assertTrue(rectangle.intersects(circle));
     }
 
+    /**
+     * check if a rectangle intersects with a circle
+     * they should not intersect, since one ball is at position 4,4
+     */
     @Test
     public void intersectCircle2(){
-        Circle circle = new Circle(6);
-        circle.setPosition(10,40);
-        Rectangle rectangle = new Rectangle(4,5);
+        Circle circle = new Circle(1);
+        circle.setPosition(4,4);
+        Rectangle rectangle = new Rectangle(2,3);
         assertFalse(rectangle.intersects(circle));
     }
 
+    /**
+     * check if a rectangle intersects with a rectangle
+     * they should intersect, since they are both at position 0,0
+     */
     @Test
     public void intersectRectangle(){
+        Rectangle rectangle = new Rectangle(4,5);
+        Rectangle rectangle2 = new Rectangle(1,2);
+        assertTrue(rectangle.intersects(rectangle2));
+    }
+
+    /**
+     * check if a rectangle intersects with a rectangle
+     * they should not intersect, since one ball is at position 5,5
+     */
+    @Test
+    public void intersectRectangle2(){
         Rectangle rectangle = new Rectangle(4,5);
         Rectangle rectangle2 = new Rectangle(1,2);
         rectangle.setOffset(5,5);
