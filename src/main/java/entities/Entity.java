df595f5e2f01f6e934af3fa097979bed53a2eddb
Timo van Asten <timovanasten@gmail.com>
2016-09-21 21:57:02 +0200

    Fix Checkstyle errors in Entity

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -5,79 +5,199 @@ import geometry.Shape;
 import util.KeyboardInputManager;
 import util.Sprite;
 
+/**
+ * Class containing all the functionality shared by all in-game entities.
+ */
 public abstract class Entity {
 
-	protected static KeyboardInputManager keyboard = KeyboardInputManager.getInstance();
+    /**
+     * KeyboardInputMangaer object that manages
+     * the keyboard input of the entity.
+     */
+    private static KeyboardInputManager
+            keyboard = KeyboardInputManager.getInstance();
 
-	protected Vec2d position = new Vec2d(0, 0);
-    protected Vec2d speed = new Vec2d(0, 0);
-	protected Sprite sprite = null;
-    protected Shape shape = null;
+    /**
+     * Position of the entity.
+     */
+    private Vec2d position = new Vec2d(0, 0);
+
+    /**
+     * Speed of the entity.
+     */
+    private Vec2d speed = new Vec2d(0, 0);
+
+    /**
+     * Sprite of the entity.
+     */
+    private Sprite sprite = null;
+
+    /**
+     * Shape used for collision detection of the entity.
+     */
+    private Shape shape = null;
 
     /**
      * Boolean with the current visibility state of an entity.
      */
-    protected boolean visible;
+    private boolean visible;
 
-
-	public Entity() {
-		this(0, 0);
-	}
-	
-	public Entity(double x, double y) {
-		this(new Vec2d(x, y));
-	}
-
-	public Entity(Vec2d position) {
-		setPosition(position);
-        visible = true;
-	}
-
-	public void setPosition(double x, double y) {
-		this.position.x = x;
-        this.position.y = y;
-	}
-
-	public void setPosition(Vec2d position) {
-	    setPosition(position.x, position.y);
-	}
-
-	public double getX() {
-	    return position.x;
+    /**
+     * Creates a new entity at position (0,0).
+     */
+    public Entity() {
+        this(0, 0);
     }
 
+    /**
+     * Creates a new entity at position (x,y).
+     * @param x The x coordinate of the entity
+     * @param y The y coordinate of the entity
+     */
+    public Entity(double x, double y) {
+        this(new Vec2d(x, y));
+    }
+
+    /**
+     * Creates a new entity at the specified position.
+     * @param position A Vec2D containing the coordinates of the position
+     */
+    public Entity(Vec2d position) {
+        setPosition(position);
+        visible = true;
+    }
+
+    /**
+     * Set the position of the Entity.
+     * @param x The x coordinate of the entity
+     * @param y The y coordinate of the entity
+     */
+    public void setPosition(double x, double y) {
+        this.position.x = x;
+        this.position.y = y;
+    }
+
+    /**
+     * Set the position of the entity.
+     * @param position A Vec2D containing the coordinates of the position
+     */
+    public void setPosition(Vec2d position) {
+        setPosition(position.x, position.y);
+    }
+
+    /**
+     * @return The x position of the entity.
+     */
+    public double getX() {
+        return position.x;
+    }
+
+    /**
+     * @return The y position of the entity.
+     */
     public double getY() {
         return position.y;
     }
 
-	public void setSpeed(double x, double y) {
-		speed.x = x;
+    /**
+     * Sets the speed of the entity.
+     * @param x The horizontal speed
+     * @param y The vertical speed
+     */
+    public void setSpeed(double x, double y) {
+        speed.x = x;
         speed.y = y;
-	}
-
-	public void setSpeed(Vec2d speed) {
-	    setSpeed(speed.x, speed.y);
-	}
-	
-	public void update(double timeDifference) {
-		if (sprite != null) {
-			sprite.update(timeDifference);
-		}
-	}
-
-	public boolean intersects(Entity entity) {
-	    if (shape == null || entity.shape == null) {
-	        return false;
-        }
-
-        return shape.intersects(entity.shape);
     }
 
-    public void collideWith(Entity entity) {}
-	
-	public void draw() {
-		if (sprite != null && visible) {
-			sprite.draw(position);
-		}
-	}
+    /**
+     * Sets the speed of the entity.
+     * @param speed a Vec2D containing the x and y speeds of the entity.
+     */
+    public void setSpeed(Vec2d speed) {
+        setSpeed(speed.x, speed.y);
+    }
+
+    /**
+     * Updates the entity's sprite if it has one.
+     * @param timeDifference The time elapsed since the last time the method was called
+     */
+    public void update(double timeDifference) {
+        if (sprite != null) {
+            sprite.update(timeDifference);
+        }
+    }
+
+    /**
+     * Checks if the entity intersects with another entity.
+     * @param entity The entity you want to check for intersection
+     * @return a boolean indicating if the entity intersects with the other entity
+     */
+    public boolean intersects(Entity entity) {
+        return !(shape == null || entity.shape == null) && shape.intersects(entity.shape);
+
+    }
+
+    //TODO: Implement or delete this method.
+    /**
+     * This does nothing?
+     * @param entity the entity to do nothing with.
+     */
+    public void collideWith(Entity entity) { }
+
+    /**
+     * Draws the entity to the screen.
+     */
+    public void draw() {
+        if (sprite != null && visible) {
+            sprite.draw(position);
+        }
+    }
+
+    /**
+     * Returns the keyboard manager of the entity.
+     * @return the keyboardmanager
+     */
+    public static KeyboardInputManager getKeyboard() {
+        return keyboard;
+    }
+
+    /**
+     * Returns the position of the entity.
+     * @return the position
+     */
+    public Vec2d getPosition() {
+        return position;
+    }
+
+    /**
+     * Returns the speed of the entity.
+     * @return the speed of the entity
+     */
+    public Vec2d getSpeed() {
+        return speed;
+    }
+
+    /**
+     * Returns the sprite of the entity.
+     * @return the sprite of the entity
+     */
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    /**
+     * Returns The shape that is used for collisions of the entity.
+     * @return The shape that is used for collisions of the entity
+     */
+    public Shape getShape() {
+        return shape;
+    }
+
+    /**
+     * Returns the boolean indicating if the sprite is visable.
+     * @return the boolean indicating if the sprite is visable
+     */
+    public boolean isVisible() {
+        return visible;
+    }
 }
