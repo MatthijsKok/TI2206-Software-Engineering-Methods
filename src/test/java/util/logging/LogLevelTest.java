aa8c0243911c8c684cbcc1e0154fcb1f31e8998f
Dana van Hassel <dvanhassel@student.tudelft.nl>
2016-09-28 10:55:14 +0200

    basis for testing logging

---
+++ src/test/java/util/logging/LogLevelTest.java
@@ -0,0 +1,26 @@
+package util.logging;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by dana on 28/09/2016.
+ * Class that tests LogLevel
+ */
+public class LogLevelTest {
+    LogLevel mockLogLevel = mock(LogLevel.class);
+
+    @Before
+    public void setUp(){
+    }
+
+    @Test
+    public void getNameTest(){
+    }
+
+    @Test
+    public void getValueTest(){
+    }
+}
---
+++ src/test/java/util/logging/LogRecordTest.java
@@ -0,0 +1,19 @@
+package util.logging;
+
+import org.junit.Test;
+
+/**
+ * Created by dana on 28/09/2016.
+ */
+public class LogRecordTest {
+
+    @Test
+    public void FormatTest(){
+//        Dit kan weer niet, want LogLevel is private en ik kan geen loglevel object aanmaken.
+//        Loglevel loglevel = new LogLevel();
+//        LogRecord(loglevel, "sourceClassName", "sourceMethodName", "message", 0.001);
+    }
+    @Test
+    public void formatTest(){
+    }
+}
