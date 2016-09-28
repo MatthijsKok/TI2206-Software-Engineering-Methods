aa4b453489cbb9f3664f1e8df6174953ec2e8492
Wouter Raateland <wouterraateland@gmail.com>
2016-09-28 12:01:53 +0200

    Refactor Logger to be more accessible for tests and to fit more into the singleton pattern. Write test for info logger.

--- src/test/java/util/logging/LoggerTest.java
+++ src/test/java/util/logging/LoggerTest.java
@@ -1,26 +1,29 @@
 package util.logging;
 
+import org.junit.BeforeClass;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by dana on 27/09/2016.
- * This class tests logger
+ * This tests test the Logger class.
  */
 public class LoggerTest {
 
-    private static final String LOG_MESSAGE = "hallo hallo, een log berichtje hier.";
+    private static final String LOG_MESSAGE = "hello hello, check out this BEAUTIFUL logger!";
 
-    private Logger logger;
-    private ArrayList<LogRecord> logRecords = new ArrayList<>();
+    private static Logger logger;
+
+    @BeforeClass
+    public static void setUpClass() {
+        logger = Logger.getInstance();
+    }
 
     @Before
     public void setUp() {
-        logger = Logger.getInstance();
+        logger.purgeLogRecords();
     }
 
     @Test
@@ -51,6 +54,7 @@ public class LoggerTest {
     public void infoTest() {
         logger.setLevel(LogLevel.INFO);
         logger.info(LOG_MESSAGE);
+        assertEquals(logger.getLogRecords().size(), 1);
     }
 
 }
