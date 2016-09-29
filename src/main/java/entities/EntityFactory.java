560e0173faa7f6e4cb97517bc4ba973a8ac48bc1
Sterre Noorthoek <snoorthoek@student.tudelft.nl>
2016-09-29 16:45:10 +0200

    new files LevelFactory and EntityFactory

---
+++ src/main/java/entities/EntityFactory.java
@@ -0,0 +1,7 @@
+package entities;
+
+/**
+ * Created by Sterre on 29-09-16.
+ */
+public class EntityFactory {
+}
---
+++ src/main/java/game/LevelFactory.java
@@ -0,0 +1,19 @@
+package game;
+
+/**
+ * Created by Sterre on 29-09-16.
+ */
+public class LevelFactory {
+
+    private static LevelFactory obj = null;
+
+    public readJSONFile (String file){
+        //JSONParser parser = new JSONParser();
+
+
+    }
+
+
+
+
+}
