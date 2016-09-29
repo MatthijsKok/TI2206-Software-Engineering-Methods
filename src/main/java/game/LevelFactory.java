4211c4455f900f373348f905348e9a7d014ca8ed
Sterre Noorthoek <snoorthoek@student.tudelft.nl>
2016-09-29 17:42:14 +0200

    method readJSONFile in LevelFactory

--- src/main/java/game/LevelFactory.java
+++ src/main/java/game/LevelFactory.java
@@ -9,22 +9,32 @@ public class LevelFactory {
 
     private static LevelFactory obj = null;
 
-    public String readJSONFile () {
-        String filename = "src/main/resources/levels/";
-        filename = filename + "level1.json";
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filename));
-            System.out.println("test");
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        return "test";
+    public static void main(String[] args) {
+        readJSONFile("level1.json");
 
     }
 
+    public static String readJSONFile (String filename) {
+        filename = "src/main/resources/levels/"+ filename;
+        String result = "";
 
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line);
+                line = bufferedReader.readLine();
+            }
+            result = stringBuilder.toString();
+            System.out.println(result);
+        }
 
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 
 }
