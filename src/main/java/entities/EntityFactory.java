e50c81eb65358e7422a8d5276f38e56c4be07433
Dana van Hassel <dvanhassel@student.tudelft.nl>
2016-09-29 22:14:26 +0200

    entity factory

--- src/main/java/entities/EntityFactory.java
+++ src/main/java/entities/EntityFactory.java
@@ -1,7 +1,23 @@
 package entities;
 
+import game.LevelFactory;
+import org.json.JSONObject;
+
 /**
  * Created by Sterre on 29-09-16.
  */
 public class EntityFactory {
+
+    public static void main(String[] args) {
+//        readJSONFile("level1.json");
+        parseJSONString("level1.json");
+    }
+
+    public static void parseJSONString (String filename){
+        filename = LevelFactory.readJSONFile(filename);
+
+        JSONObject jObject = new JSONObject(filename);
+    }
+
+
 }
