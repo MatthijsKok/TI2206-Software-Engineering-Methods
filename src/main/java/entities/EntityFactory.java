5289d41a8648cc58d5439df780fb2f7564ee68ea
Dana van Hassel <dvanhassel@student.tudelft.nl>
2016-09-30 12:13:56 +0200

    entity factory, parsing type, x and y, and the attributes. The objects are not linked to actual Level and entity objects yet.

--- src/main/java/entities/EntityFactory.java
+++ src/main/java/entities/EntityFactory.java
@@ -4,30 +4,34 @@ import game.LevelFactory;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.FileNotFoundException;
+
 /**
  * Created by Sterre on 29-09-16.
  */
 public class EntityFactory {
 
+    /**
+     * Parses the JSONArray entities in entity objects
+     * @param entities
+     */
     public static void parseJSONString (JSONArray entities){
-
         System.out.println("---- EntityFactory parser: ----");
-
         final int n = entities.length();
         for (int i = 0; i < n; ++i) {
-            final JSONObject entitie = entities.getJSONObject(i);
+            JSONObject entitie = entities.getJSONObject(i);
+            JSONObject attributes = entitie.getJSONObject("attributes");
 
-            System.out.println(entitie.getString("type"));
-            System.out.println(entitie.getInt("x"));
-            System.out.println(entitie.getInt("y"));
-
-//            final JSONArray attributes = entitie.getJSONArray("attributes");
-//            final int k = attributes.length();
-//            for (int j = 0; j < k; ++j){
-//                final JSONObject attribute = attributes.getJSONObject(j);
-//
-//                System.out.println("size: "+ entitie.getInt("size"));
-//            }
+            //Syso is for testing purposes
+            System.out.println("Type: " + entitie.getString("type"));
+            System.out.println("x:    " + entitie.getInt("x"));
+            System.out.println("y:    " + entitie.getInt("y"));
+            try {
+                System.out.println("size: " + attributes.getInt("size"));
+            }
+            catch (Exception e){
+                System.out.println("no size available");
+            }
             System.out.println();
         }
     }
