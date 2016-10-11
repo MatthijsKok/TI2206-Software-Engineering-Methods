7b1963d636c083699dd8b880b3f4c790bed34ed8
Matthijs Kok <matthijs.kok@me.com>
2016-10-11 09:22:00 +0200

    Test LogLevel

--- src/test/java/util/logging/LogLevelTest.java
+++ src/test/java/util/logging/LogLevelTest.java
@@ -1,25 +1,34 @@
 package util.logging;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
 
 /**
- * Created by dana on 28/09/2016.
- * Class that tests LogLevel
+ * Class that tests LogLevel.
  */
 public class LogLevelTest {
 
-    @Before
-    public void setUp(){
-    }
-
     @Test
     public void getNameTest(){
+        assertEquals(LogLevel.OFF.getName(), "OFF");
+        assertEquals(LogLevel.FATAL.getName(), "FATAL");
+        assertEquals(LogLevel.ERROR.getName(), "ERROR");
+        assertEquals(LogLevel.WARN.getName(), "WARN");
+        assertEquals(LogLevel.INFO.getName(), "INFO");
+        assertEquals(LogLevel.DEBUG.getName(), "DEBUG");
+        assertEquals(LogLevel.TRACE.getName(), "TRACE");
+        assertEquals(LogLevel.ALL.getName(), "ALL");
     }
 
     @Test
     public void getValueTest(){
+        assertEquals(LogLevel.OFF.getValue(), Integer.MAX_VALUE);
+        assertEquals(LogLevel.FATAL.getValue(), 1000);
+        assertEquals(LogLevel.ERROR.getValue(), 900);
+        assertEquals(LogLevel.WARN.getValue(), 800);
+        assertEquals(LogLevel.INFO.getValue(), 700);
+        assertEquals(LogLevel.DEBUG.getValue(), 600);
+        assertEquals(LogLevel.TRACE.getValue(), 500);
+        assertEquals(LogLevel.ALL.getValue(), Integer.MIN_VALUE);
     }
 }
