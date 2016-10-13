c805a0314fd78d3a8884976eba4fc0c706a19e99
Sterre Noorthoek <sterrechavon@hotmail.com>
2016-10-13 16:15:08 +0200

    Added Javadoc comments

--- src/main/java/entities/powerups/ExtraTime.java
+++ src/main/java/entities/powerups/ExtraTime.java
@@ -2,16 +2,30 @@ package entities.powerups;
 
 public class ExtraTime implements StaticPowerUp {
 
+    /**
+     * Time the power up lasts.
+     */
     int timeRemaining;
 
+    /**
+     * Returns the time the effect of the power up will still last.
+     */
+    @Override
     public int getTimeRemaining() {
         return 0;
     }
 
+    /**
+     * Decreases the time the effect of the power up will still last.
+     */
+    @Override
     public int decreaseTimeRemaining() {
         return 0;
     }
 
+    /**
+     * Applies the effect of the specific power up.
+     */
     @Override
     public void applyEffect() {
     }
