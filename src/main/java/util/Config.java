42916613e94e374e002c85bc218a5c1aa36a37a0
Matthijs Kok <matthijs.kok@me.com>
2016-10-18 11:57:56 +0200

    Rewrite Config to use default settings as well.

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -1,5 +1,7 @@
 package util;
 
+import util.logging.Logger;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,25 +13,50 @@ import java.util.Properties;
 public final class Config {
 
     /**
+     * The logger access point to which everything will be logged.
+     */
+    private static final Logger LOGGER = Logger.getInstance();
+
+    /**
+     * The properties object wherein all default config properties are defined.
+     */
+    private static final Properties DEFAULT_PROPERTIES;
+
+    /**
      * The properties object wherein all config properties will be loaded.
      */
     private static final Properties PROPERTIES;
+
     /**
      * The file containing all config properties.
      */
-    private static final String PROP_FILE_NAME = "src/main/resources/config.properties";
+    private static final String PROPERTIES_FILE_NAME = "settings.properties";
+
+    /**
+     * The file containing all default config properties.
+     */
+    private static final String DEFAULT_PROPERTIES_FILE_NAME = "default_settings.properties";
 
     static {
-        Properties fallback = new Properties();
-        fallback.put("key", "default");
+        DEFAULT_PROPERTIES = new Properties();
+        try (InputStream inputStream = new FileInputStream(DEFAULT_PROPERTIES_FILE_NAME)) {
+            DEFAULT_PROPERTIES.load(inputStream);
+            LOGGER.info("Default settings loaded.");
+        } catch (IOException e) {
+            LOGGER.error("Default settings could not be loaded.");
+            e.printStackTrace();
+        }
 
-        PROPERTIES = new Properties(fallback);
-        try {
-            try (InputStream stream = new FileInputStream(PROP_FILE_NAME)) {
-                PROPERTIES.load(stream);
-            }
-        } catch (IOException ex) {
-            System.exit(1);
+        // Load Default Properties into Properties, before overriding with config.
+        // So that all values that are not defined are standard.
+        PROPERTIES = DEFAULT_PROPERTIES;
+
+        try (InputStream inputStream = new FileInputStream(PROPERTIES_FILE_NAME)) {
+            PROPERTIES.load(inputStream);
+            LOGGER.info("Settings loaded.");
+        } catch (IOException e) {
+            LOGGER.warn("Settings could not be loaded. Using default settings instead.");
+            e.printStackTrace();
         }
     }
 
