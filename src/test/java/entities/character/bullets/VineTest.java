d70922d2efdcb23e01c5d2e6dc5ab8f58a886b83
Wouter Raateland <wouterraateland@gmail.com>
2016-11-02 15:28:07 +0100

    Test entities package up to 95% line coverage

--- src/test/java/entities/character/bullets/VineTest.java
+++ src/test/java/entities/character/bullets/VineTest.java
@@ -1,5 +1,6 @@
 package entities.character.bullets;
 
+import entities.blocks.WallBlock;
 import main.BubbleTroubleApplicationTest;
 import com.sun.javafx.geom.Vec2d;
 import entities.balls.ColoredBall;
@@ -21,35 +22,21 @@ import static org.mockito.Mockito.when;
 public class VineTest extends BubbleTroubleApplicationTest {
 
     private Vec2d position1 = new Vec2d(100, 300);
-    private Vec2d position2 = new Vec2d(0, 0);
     private Character mockedCharacter = Mockito.mock(Character.class);
     private Gun mockedGun = Mockito.mock(Gun.class);
-    private Vine vine1, vine2;
+    private Vine vine1;
 
 
     @Before
     public void setUp() {
         when(mockedCharacter.getGun()).thenReturn(mockedGun);
         vine1 = new Vine(position1, mockedCharacter);
-        vine2 = new Vine(position2, mockedCharacter);
     }
 
     @Test
     public void testMovingAfterInstantiation() {
         assertThat(vine1.getYSpeed(), lessThan(0.d));
     }
-
-    /*@Test
-    public void testUpdateYGreaterThanZero() {
-        vine1.update(0);
-        verify(mockedGun, times(0)).bulletDied();
-    }
-
-    @Test
-    public void testUpdateYSmallerThanZero() {
-        vine2.update(0);
-        verify(mockedGun, times(1)).bulletDied();
-    }*/
 
     @Test
     public void testCollisionWithBall() {
@@ -59,11 +46,11 @@ public class VineTest extends BubbleTroubleApplicationTest {
         verify(mockedCharacter, times(1)).increaseScore(300);
     }
 
-//    @Test
-//    public void testCollisionWithOtherEntity() {
-//        WallBlock otherEntity = new WallBlock(new Vec2d(300, 200));
-//        vine1.collideWith(otherEntity);
-//
-//        verify(mockedCharacter, times(0)).vineRemoved();
-//    }
+    @Test
+    public void testCollisionWithBlock() {
+        WallBlock block = Mockito.mock(WallBlock.class);
+        vine1.collideWith(block);
+
+        verify(mockedGun, times(1)).bulletDied();
+    }
 }
